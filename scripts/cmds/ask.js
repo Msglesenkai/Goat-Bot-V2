42b43490d0cce70ceb0652db334706c0d3d2b41e
Msglesenkai <165266091+Msglesenkai@users.noreply.github.com>
2024-04-07 11:31:36 +0100

    ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -47,14 +47,14 @@ module.exports = {
       }
       const prompt = event.body.substring(prefix.length).trim();
       if (!prompt) {
-        await message.reply("🔖𝘚𝘢𝘭𝘶𝘵 𝘮𝘰𝘪 𝘤'𝘦𝘴𝘵 𝘓𝘶𝘯𝘢 𝘘𝘶𝘦𝘭 𝘦𝘴𝘵 𝘷𝘰𝘵𝘳𝘦 𝘲𝘶𝘦𝘴𝘵𝘪𝘰𝘯❓");
+        await message.reply("𝘚𝘢𝘭𝘶𝘵 𝘮𝘰𝘪 𝘤'𝘦𝘴𝘵 Sënkaï 𝘘𝘶𝘦𝘭 𝘦𝘴𝘵 𝘷𝘰𝘵𝘳𝘦 𝘲𝘶𝘦𝘴𝘵𝘪𝘰𝘯❓");
         return;
       }
       const senderID = event.senderID;
       const senderInfo = await api.getUserInfo([senderID]);
       const senderName = senderInfo[senderID].name;
       const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-      const answer = `🔖𝙇𝙐𝙉𝘼🙂:\n──────────── \n${response.data.answer} 𝙈𝙀𝙍𝘿𝙄🔖`;
+      const answer = `Senkai😒:\n──────────── \n${response.data.answer} Kïng`;
 
       //apply const font to each letter in the answer
       let formattedAnswer = "";
